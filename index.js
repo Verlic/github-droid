4a1af393e1d92cbf7b8f2d5382fb36d3efb6c539
Hernan Meydac <hernan.meydac@auth0.com>
2016-10-19 22:13:55 -0300

    Adds merge function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+var moment = require('moment');
 var GitHubApi = require('github'),
   promise = require('promise'),
   github = new GitHubApi({
     // required
     version: "3.0.0",
     // optional
-    debug: true,
+    debug: false,
     protocol: "https",
     host: "api.github.com",
     pathPrefix: "",
@@ -45,6 +46,51 @@ module.exports = function(context) {
         }).catch(function(err) {
           return res.text('Unable to assign user to Pull Request.\n```' + JSON.stringify(err, null, 2) + '```').send();
         });
+    },
+    merged: function(req, res) {
+      var repo = req.params.repo;
+      var relative = req.params.relative.toLowerCase();
+      relative = relative === 'today' || relative === 'yesterday' ? 'day' : relative;
+      var relativeDate = moment().utc().subtract(1, relative).format('YYYY-MM-DD');
+      var payload = {
+        q: `is:merged merged:>=${relativeDate} repo:${organization}/${repo}`
+      };
+
+      var prs = [];
+      function outputPrs() {
+        var attachment = {
+          title: `Merged PRs for ${repo} - 1 ${relative}`,
+          fallback: `Merged PRs for ${repo}`,
+          'mrkdwn_i': ['fields'],
+          color: '#36a64f',
+          fields: []
+        };
+
+        prs.forEach(function(pr) {
+          attachment.fields.push({value: `<${pr.html_url}|${pr.title}>`, short: false});
+        });
+
+        if (prs.length === 0) {
+          attachment.text = 'No PRs merged found';
+        }
+
+        return res.attachment(attachment).send();
+      }
+
+      function getPrs(err, response) {
+        if (err) {
+          return res.text('Unable to retrieve the merged PRs\n```' + JSON.stringify(err) + '```').send();
+        }
+
+        prs = prs.concat(response.items);
+        if (github.hasNextPage(response)) {
+          github.getNextPage(response, getPrs);
+        } else {
+          outputPrs();
+        }
+      }
+
+      github.search.issues(payload, getPrs);
     },
     help: function(req, res) {
       return res.text('Use `assign {repo}#{pr-number} to {github-user}` to assign a user to a Pull Request').send();
